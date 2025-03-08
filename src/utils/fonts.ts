d57e2b0d1c10f7e9f434996e707465f6bc7f6f05
Abdulbassit Abdullahi <funabab@gmail.com>
2025-03-08 21:57:05 +0100

    feat: setup theme and storybook

---
+++ src/utils/fonts.ts
@@ -0,0 +1,11 @@
+import { Karla, Space_Grotesk } from "next/font/google";
+
+export const fontKarla = Karla({
+  variable: "--font-karla",
+  subsets: ["latin"]
+});
+
+export const fontSpaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"]
+});
