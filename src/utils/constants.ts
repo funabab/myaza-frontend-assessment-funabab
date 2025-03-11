d428873591e9fcbee4d3d251fdede16eda80e7d3
Abdulbassit Abdullahi <funabab@gmail.com>
2025-03-11 16:23:08 +0100

    refactor: minor update to site url logic

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const SITE_URL = `${process.env.NEXT_PUBLIC_SITE || "http://localhost:3000"}`;
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE;
